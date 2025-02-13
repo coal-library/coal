8d6bb3e2cd74aaa21080a32eba2076f9174c3fbc
Guilhem Saurel <guilhem.saurel@laas.fr>
2025-02-13 15:09:29 +0100

    Nix: BUILD_STANDALONE_PYTHON_INTERFACE

--- flake.nix
+++ flake.nix
@@ -17,11 +17,43 @@
             type = "app";
             program = pkgs.python3.withPackages (_: [ self'.packages.default ]);
           };
-          devShells.default = pkgs.mkShell { inputsFrom = [ self'.packages.default ]; };
           packages = {
             default = self'.packages.coal;
-            coal = pkgs.python3Packages.coal.overrideAttrs (super: {
-              cmakeFlags = super.cmakeFlags ++ [ "-DCOAL_DISABLE_HPP_FCL_WARNINGS=ON" ];
+            coal = pkgs.python3Packages.toPythonModule (
+              (pkgs.coal.override { pythonSupport = true; }).overrideAttrs (super: {
+                cmakeFlags = super.cmakeFlags ++ [ "-DCOAL_DISABLE_HPP_FCL_WARNINGS=ON" ];
+                src = pkgs.lib.fileset.toSource {
+                  root = ./.;
+                  fileset = pkgs.lib.fileset.unions [
+                    ./CMakeLists.txt
+                    ./doc
+                    ./hpp-fclConfig.cmake
+                    ./include
+                    ./package.xml
+                    ./python
+                    ./src
+                    ./test
+                  ];
+                };
+              })
+            );
+            coal-cpp = (self'.packages.coal.override { pythonSupport = false; }).overrideAttrs (super: {
+              src = pkgs.lib.fileset.toSource {
+                root = ./.;
+                fileset = pkgs.lib.fileset.unions [
+                  ./CMakeLists.txt
+                  ./doc
+                  ./hpp-fclConfig.cmake
+                  ./include
+                  ./package.xml
+                  # ./python
+                  ./src
+                  ./test
+                ];
+              };
+            });
+            coal-py = (self'.packages.coal.override { pythonSupport = true; }).overrideAttrs (super: {
+              cmakeFlags = super.cmakeFlags ++ [ "-DBUILD_STANDALONE_PYTHON_INTERFACE=ON" ];
               src = pkgs.lib.fileset.toSource {
                 root = ./.;
                 fileset = pkgs.lib.fileset.unions [
@@ -31,10 +63,13 @@
                   ./include
                   ./package.xml
                   ./python
-                  ./src
+                  # ./src
                   ./test
                 ];
               };
+              propagatedBuildInputs = super.propagatedBuildInputs ++ [
+                self'.packages.coal-cpp
+              ];
             });
           };
         };
