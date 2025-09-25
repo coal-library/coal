56c4b52db954904055657a751b1328938da64091
Joris Vaillant <joris.vaillant@inria.fr>
2025-09-25 17:29:02 +0200

    bench: Use ILP to accelerate computation

--- benchmark/convex_support_highway.cpp
+++ benchmark/convex_support_highway.cpp
@@ -26,17 +26,38 @@ Scalar _support(const Scalar* HWY_RESTRICT x, const Scalar* HWY_RESTRICT y,
   V y_dir_v = hn::Set(d, y_dir);
   V z_dir_v = hn::Set(d, z_dir);
 
-  V max_dot_v = hn::Set(d, std::numeric_limits<Scalar>::min());
+  V max_dot_v_ilp1 = hn::Set(d, std::numeric_limits<Scalar>::min());
+  V max_dot_v_ilp2 = hn::Set(d, std::numeric_limits<Scalar>::min());
   size_t i = 0;
+  for (; i + N * 2 <= count; i += N * 2) {
+    const V x_v_ilp1 = hn::Load(d, x + i);
+    const V dot_v1_ilp1 = x_v_ilp1 * x_dir_v;
+    const V y_v_ilp1 = hn::Load(d, y + i);
+    const V dot_v2_ilp1 = hn::MulAdd(y_v_ilp1, y_dir_v, dot_v1_ilp1);
+    const V z_v_ilp1 = hn::Load(d, z + i);
+    const V dot_v_ilp1 = hn::MulAdd(z_v_ilp1, z_dir_v, dot_v2_ilp1);
+
+    max_dot_v_ilp1 = hn::Max(max_dot_v_ilp1, dot_v_ilp1);
+
+    const V x_v_ilp2 = hn::Load(d, x + i);
+    const V dot_v1_ilp2 = x_v_ilp2 * x_dir_v;
+    const V y_v_ilp2 = hn::Load(d, y + i);
+    const V dot_v2_ilp2 = hn::MulAdd(y_v_ilp2, y_dir_v, dot_v1_ilp2);
+    const V z_v_ilp2 = hn::Load(d, z + i);
+    const V dot_v_ilp2 = hn::MulAdd(z_v_ilp2, z_dir_v, dot_v2_ilp2);
+
+    max_dot_v_ilp2 = hn::Max(max_dot_v_ilp2, dot_v_ilp2);
+  }
+  V max_dot_v = hn::Max(max_dot_v_ilp1, max_dot_v_ilp2);
+
   for (; i + N <= count; i += N) {
     const V x_v = hn::Load(d, x + i);
-    const V x_dot_v = x_v * x_dir_v;
+    const V dot_v1 = x_v * x_dir_v;
     const V y_v = hn::Load(d, y + i);
-    const V y_dot_v = y_v * y_dir_v;
+    const V dot_v2 = hn::MulAdd(y_v, y_dir_v, dot_v1);
     const V z_v = hn::Load(d, z + i);
-    const V z_dot_v = z_v * z_dir_v;
+    const V dot_v = hn::MulAdd(z_v, z_dir_v, dot_v2);
 
-    const V dot_v = x_dot_v + y_dot_v + z_dot_v;
     max_dot_v = hn::Max(max_dot_v, dot_v);
   }
 
