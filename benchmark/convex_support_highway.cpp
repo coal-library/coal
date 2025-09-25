5468311838738415781d1cd13ca5f0c544ea3e6b
Joris Vaillant <joris.vaillant@inria.fr>
2025-09-25 20:57:59 +0200

    bench: Fix ILP code and add 4 way test

--- benchmark/convex_support_highway.cpp
+++ benchmark/convex_support_highway.cpp
@@ -28,7 +28,10 @@ Scalar _support(const Scalar* HWY_RESTRICT x, const Scalar* HWY_RESTRICT y,
 
   V max_dot_v_ilp1 = hn::Set(d, std::numeric_limits<Scalar>::min());
   V max_dot_v_ilp2 = hn::Set(d, std::numeric_limits<Scalar>::min());
+  // V max_dot_v_ilp3 = hn::Set(d, std::numeric_limits<Scalar>::min());
+  // V max_dot_v_ilp4 = hn::Set(d, std::numeric_limits<Scalar>::min());
   size_t i = 0;
+  // for (; i + N * 4 <= count; i += N * 4) {
   for (; i + N * 2 <= count; i += N * 2) {
     const V x_v_ilp1 = hn::Load(d, x + i);
     const V dot_v1_ilp1 = x_v_ilp1 * x_dir_v;
@@ -39,15 +42,36 @@ Scalar _support(const Scalar* HWY_RESTRICT x, const Scalar* HWY_RESTRICT y,
 
     max_dot_v_ilp1 = hn::Max(max_dot_v_ilp1, dot_v_ilp1);
 
-    const V x_v_ilp2 = hn::Load(d, x + i);
+    const V x_v_ilp2 = hn::Load(d, x + i + 1 * N);
     const V dot_v1_ilp2 = x_v_ilp2 * x_dir_v;
-    const V y_v_ilp2 = hn::Load(d, y + i);
+    const V y_v_ilp2 = hn::Load(d, y + i + 1 * N);
     const V dot_v2_ilp2 = hn::MulAdd(y_v_ilp2, y_dir_v, dot_v1_ilp2);
-    const V z_v_ilp2 = hn::Load(d, z + i);
+    const V z_v_ilp2 = hn::Load(d, z + i + 1 * N);
     const V dot_v_ilp2 = hn::MulAdd(z_v_ilp2, z_dir_v, dot_v2_ilp2);
 
     max_dot_v_ilp2 = hn::Max(max_dot_v_ilp2, dot_v_ilp2);
+
+    // const V x_v_ilp3 = hn::Load(d, x + i + 2 * N);
+    // const V dot_v1_ilp3 = x_v_ilp3 * x_dir_v;
+    // const V y_v_ilp3 = hn::Load(d, y + i + 2 * N);
+    // const V dot_v2_ilp3 = hn::MulAdd(y_v_ilp3, y_dir_v, dot_v1_ilp3);
+    // const V z_v_ilp3 = hn::Load(d, z + i + 2 * N);
+    // const V dot_v_ilp3 = hn::MulAdd(z_v_ilp3, z_dir_v, dot_v2_ilp3);
+    //
+    // max_dot_v_ilp3 = hn::Max(max_dot_v_ilp3, dot_v_ilp3);
+    //
+    // const V x_v_ilp4 = hn::Load(d, x + i + 3 * N);
+    // const V dot_v1_ilp4 = x_v_ilp4 * x_dir_v;
+    // const V y_v_ilp4 = hn::Load(d, y + i + 3 * N);
+    // const V dot_v2_ilp4 = hn::MulAdd(y_v_ilp4, y_dir_v, dot_v1_ilp4);
+    // const V z_v_ilp4 = hn::Load(d, z + i + 3 * N);
+    // const V dot_v_ilp4 = hn::MulAdd(z_v_ilp4, z_dir_v, dot_v2_ilp4);
+    //
+    // max_dot_v_ilp4 = hn::Max(max_dot_v_ilp4, dot_v_ilp4);
   }
+  // V max_dot_v1 = hn::Max(max_dot_v_ilp1, max_dot_v_ilp2);
+  // V max_dot_v2 = hn::Max(max_dot_v_ilp3, max_dot_v_ilp4);
+  // V max_dot_v = hn::Max(max_dot_v1, max_dot_v2);
   V max_dot_v = hn::Max(max_dot_v_ilp1, max_dot_v_ilp2);
 
   for (; i + N <= count; i += N) {
